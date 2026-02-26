a140212cbc9cceee1c12b91b8869afbb4f63ff11
Dede Iskandar <d3215k@gmail.com>
2026-02-26 14:33:58 +0700

    wip

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
     plugins: [
         tailwindcss(),
         laravel({
-            input: ["resources/css/app.css", "resources/js/app.js"],
+            input: ["resources/css/app.css", "resources/js/app.js", "resources/css/filament/admin/theme.css"],
             refresh: [...refreshPaths, "app/Livewire/**"],
         }),
     ],
